c283b7e859cadd9e1972a2eadeb9f798fe7c2903
Francisco Javier González Navarro <f.j.gonzalez.n@gmail.com>
2014-06-10 19:35:38 +0200

    Finished transitions between pages!

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,24 +1,29 @@
 
 // Show and hidden flash message
 $(document).ready(function(){
-  $('#msg_flash').delay(2000).fadeOut('slow');
-  setTimeout(function (){
-      $('#contenido').slideDown('3000');
-      $('#imagen-cargar').slideUp('3000');
-  }, 1000)
-  $("a").click(function (e){
-      e.preventDefault();
-      var to = $(this).attr("href");
-      $("#contenido").slideUp("3000");
-      alto = $("#imagen-precargar").height();
-      $("#imagen-precargar").slideDown("3000", function (){
-          $("#imagen-precargar").animate({
-              marginTop: -alto
-          }, 1500, function (){
-             window.location.assign(to); 
-          });
-      });
-  });
+    $('#msg_flash').delay(2000).fadeOut('slow');
+    
+    altura = $("imagen-cargar").height()/2;
+    $("#imagen-cargar").animate({
+        marginTop: + altura
+    }, 500, function (){
+        $('#contenido').slideDown('3000');
+        $('#imagen-cargar').slideUp('3000');
+    });
+    $("a").click(function (e){
+        e.preventDefault();
+        var to = $(this).attr("href");
+        $("#contenido").slideUp("3000");
+        alto = $("#imagen-precargar").height();
+        $("#imagen-precargar").slideDown("3000", function (){
+            $("body").css("background-image", "none");
+            $("#imagen-precargar").animate({
+                marginTop: -alto
+            }, 500, function (){
+                window.location.assign(to); 
+            });
+        });
+    });
 });
 
 
