2868626f2c819f424cc6345c0d61320cda9b2adc
Francisco Javier González Navarro <f.j.gonzalez.n@gmail.com>
2014-06-10 22:10:13 +0200

    some animations bugs and footer style fixed

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,35 +1,38 @@
 
 // Show and hidden flash message
-$(document).ready(function(){
+$(document).ready(function() {
     $('#msg_flash').delay(2000).fadeOut('slow');
-    
-    altura = $("imagen-cargar").height()/2;
+    altura = $(window).height();
+    $("#subcontenido").css("min-height", $(window).height() - 250);
+    altura = $("imagen-cargar").height() / 2;
     $("#imagen-cargar").animate({
-        marginTop: + altura
-    }, 500, function (){
+        marginTop: +altura
+    }, 500, function() {
         $('#contenido').slideDown('3000');
         $('#imagen-cargar').slideUp('3000');
     });
-    $("a").click(function (e){
-        e.preventDefault();
-        var to = $(this).attr("href");
-        $("#contenido").slideUp("3000");
-        alto = $("#imagen-precargar").height();
-        $("#imagen-precargar").slideDown("3000", function (){
-            $("body").css("background-image", "none");
-            $("#imagen-precargar").animate({
-                marginTop: -alto
-            }, 500, function (){
-                window.location.assign(to); 
+    $("a").click(function(e) {
+        if ($(this).attr("class") != "no-animation" && $(this).attr("href") != "#") {
+            e.preventDefault();
+            var to = $(this).attr("href");
+            $("#contenido").slideUp("3000");
+            alto = $("#imagen-precargar").height();
+            $("#imagen-precargar").slideDown("3000", function() {
+                $("body").css("background-image", "none");
+                $("#imagen-precargar").animate({
+                    marginTop: -alto
+                }, 500, function() {
+                    window.location.assign(to);
+                });
             });
-        });
+        }
     });
 });
 
 
 
-function smoothScroll(id_element, less){
-    var target = $("#"+id_element);
+function smoothScroll(id_element, less) {
+    var target = $("#" + id_element);
     $('html, body').stop().animate({
         scrollTop: (target.offset().top) - less
     }, 750);
